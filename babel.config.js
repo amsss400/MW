4f616d17e6df2cb9ffb38ae69b6f2c087f005f6a
amss400 <ahmedsignate4@outlook.fr>
2025-12-25 00:10:43 +0000

    Sauvegarde Malin-Wallet et PulseAI

--- babel.config.js
+++ babel.config.js
@@ -1,28 +1,6 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
   plugins: [
-    ['module:react-native-dotenv', {
-      moduleName: '@env',
-      path: '.env',
-      blacklist: null,
-      whitelist: null,
-      safe: true,
-      allowUndefined: true,
-    }],
     'react-native-reanimated/plugin'
   ], // required by react-native-reanimated v2 https://docs.swmansion.com/react-native-reanimated/docs/installation/,
-  env: {
-    production: {
-      plugins: [
-        ['module:react-native-dotenv', {
-          moduleName: '@env',
-          path: '.env',
-          blacklist: null,
-          whitelist: null,
-          safe: true,
-          allowUndefined: true,
-        }]
-      ]
-    }
-  }
 };
